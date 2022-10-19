ecc8fce1966d2230b2b9bbb58eb4cacb073a2d03
JayKrishnareddy <jaykrishnareddy@gmail.com>
2022-10-19 20:32:03 +0200

    Add project files.

---
+++ MVPMatch/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVPMatch.Models
+{
+    public class User
+    {
+        [Key]
+        [JsonIgnore]
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public long DespositAmount { get; set; }
+        public string Role { get; set; }
+    }
+}
